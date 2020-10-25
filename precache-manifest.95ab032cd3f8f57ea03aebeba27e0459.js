80a69fd30d86c1e517b4e1b1c4567bdd823cfac4
Albert <alb.adm.ru@gmail.com>
2020-10-25 14:33:07 +0300

    Updates

---
+++ precache-manifest.95ab032cd3f8f57ea03aebeba27e0459.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec33706e79a2481b0be1347e1b3fdd9",
+    "url": "/catchmefish/index.html"
+  },
+  {
+    "revision": "cbec6fc1d3a3b7fcabbe",
+    "url": "/catchmefish/static/css/2.1d0e61fc.chunk.css"
+  },
+  {
+    "revision": "dc02406b5fe9470a10aa",
+    "url": "/catchmefish/static/css/main.9373b821.chunk.css"
+  },
+  {
+    "revision": "cbec6fc1d3a3b7fcabbe",
+    "url": "/catchmefish/static/js/2.b9c90a74.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/catchmefish/static/js/2.b9c90a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc02406b5fe9470a10aa",
+    "url": "/catchmefish/static/js/main.2d18a5b9.chunk.js"
+  },
+  {
+    "revision": "2ac667019aab79b82d66",
+    "url": "/catchmefish/static/js/runtime-main.f46106b7.js"
+  },
+  {
+    "revision": "b27e24b098fa8073ad5e1a92f797c420",
+    "url": "/catchmefish/static/media/ContactsDelivery.b27e24b0.jpg"
+  },
+  {
+    "revision": "eddd28cbe08a2a230f666e89be6f5ffa",
+    "url": "/catchmefish/static/media/HuntingGoods.eddd28cb.jpg"
+  },
+  {
+    "revision": "394b87a7e65a05e21b1719114daa9f5f",
+    "url": "/catchmefish/static/media/PhotoForCallback.394b87a7.jpg"
+  },
+  {
+    "revision": "3c8db9254fabaa09999eb083892d616c",
+    "url": "/catchmefish/static/media/aboutUs.3c8db925.jpg"
+  },
+  {
+    "revision": "09d92b4ea052f40c37b2e2e4b785ff76",
+    "url": "/catchmefish/static/media/fm-vk-icon.09d92b4e.svg"
+  },
+  {
+    "revision": "043dfd1f0b5706c33ad826eca0ccb264",
+    "url": "/catchmefish/static/media/hm-basket-icon.043dfd1f.svg"
+  },
+  {
+    "revision": "da74469be45d9a6b2493bd7f6ded59c0",
+    "url": "/catchmefish/static/media/hm-contacts-icon.da74469b.svg"
+  },
+  {
+    "revision": "f131bb1f93da85827a0acc9019d8e2aa",
+    "url": "/catchmefish/static/media/hm-phone-icon.f131bb1f.svg"
+  },
+  {
+    "revision": "20e8ab553c721a64fe58fc3440cf7b6c",
+    "url": "/catchmefish/static/media/hm-searche-icon.20e8ab55.svg"
+  },
+  {
+    "revision": "b11f7d2279f88fbaab3ac177ea0a1320",
+    "url": "/catchmefish/static/media/iconList.b11f7d22.svg"
+  },
+  {
+    "revision": "e8d05f71cfbc929f603703d841bb1e4a",
+    "url": "/catchmefish/static/media/logo.e8d05f71.svg"
+  },
+  {
+    "revision": "81fb68dcd1337d7c8b9d56e048eae2f2",
+    "url": "/catchmefish/static/media/logoWhite.81fb68dc.svg"
+  },
+  {
+    "revision": "274b82a1c17b252b8cb63e03269eb55f",
+    "url": "/catchmefish/static/media/photoCardProduct.274b82a1.jpg"
+  },
+  {
+    "revision": "01d8d6fc479f9d629586ff3e1999ffec",
+    "url": "/catchmefish/static/media/photoOpenGood.01d8d6fc.jpg"
+  },
+  {
+    "revision": "d88595d59da70211f4459b6975f09551",
+    "url": "/catchmefish/static/media/preloader.d88595d5.svg"
+  },
+  {
+    "revision": "78d499541a84fe1c5d7fee1f7f849bf4",
+    "url": "/catchmefish/static/media/shop-icon.78d49954.svg"
+  },
+  {
+    "revision": "0554fffc6408cb3ed9bbca6e8ad4789f",
+    "url": "/catchmefish/static/media/specialOffer.0554fffc.jpg"
+  }
+]);
