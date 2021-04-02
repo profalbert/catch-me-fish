20a0a6639e0637e2d019e0aff7e1bdcdcee40ac7
Albert <alb.adm.ru@gmail.com>
2021-04-02 22:14:54 +0300

    bugs fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
-import store from './store/redux-store';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+import store from './store/redux-store'
 
 // todo: сделать BrowserRouter, а не HashRouter (лучше будет работать, так более правильно, не будет # в URL, ведь она портит внешний вид URL запроса :) )
 // это настраивается на хостинге (правильные пути)
@@ -14,15 +13,17 @@ ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>      
+  </HashRouter>,
 
-, document.getElementById('root'));
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
+serviceWorker.unregister()
 
 // basename="catchmefish"
 // "homepage": "http://albert-hosting.ru/catchmefish"
+// ИЛИ
+// "homepage": "http://profalbert.github.io/catchmefish"
